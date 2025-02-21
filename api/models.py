69d57ce78a5d9e592ecdf8ea1bcc759df9c9a8e9
Jerônimo Menezes <jeronimo@cpd.ufrgs.br>
2025-02-21 17:39:03 -0300

    apagado dbsqlite3

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Sot(models.Model):
     name = models.CharField(max_length=30, default="netbox-lab", unique=True)
     type = models.CharField(max_length=30, default="netbox")
-    hostname = models.CharField(max_length=80, default="143.54.235.23")
+    hostname = models.CharField(max_length=80, default="192.168.3.105")
     port = models.IntegerField(default=8080)
     token = models.CharField(max_length=120, default="0123456789abcdef0123456789abcdef01234567")
 
--- portal/forms.py
+++ portal/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
 class InstallForm(forms.Form): 
-    netbox_hostname = forms.CharField(max_length=80, initial='143.54.235.23')
+    netbox_hostname = forms.CharField(max_length=80, initial='192.168.3.105')
     netbox_port = forms.IntegerField(initial=8080)
     netbox_api_token = forms.CharField(max_length=120, initial='0123456789abcdef0123456789abcdef01234567')
     vlan_id = forms.IntegerField(initial=802)
     vlan_name = forms.CharField(max_length=80, initial='net2d-lab')
-    prefix4 = forms.CharField(max_length=120, initial='143.54.234.0/23')
+    prefix4 = forms.CharField(max_length=120, initial='192.168.3.0/24')
     prefix6 = forms.CharField(max_length=120, initial='2001:db8:cafe::/64')
     device_name = forms.CharField(max_length=120, initial='sw-lab-01')
-    device_address4 = forms.CharField(max_length=120, initial='143.54.235.6/23')
+    device_address4 = forms.CharField(max_length=120, initial='192.168.3.82/24')
     device_address6 = forms.CharField(max_length=120, initial='2001:db8:cafe::196/64')
     
